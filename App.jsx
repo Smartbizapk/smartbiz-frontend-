9980110bb142515de1f4107ce1e4c8d87808e420
Smartbizapk <smartbizyt335@gmail.com>
2025-06-13 22:17:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Welcome from './components/Welcome'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Smartbiz YT</h1>
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
